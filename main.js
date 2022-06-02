abefed778be115a99606abdb1879c14c0786da03
Yasushi Oiwa <iz20040cz@isk.ed.jp>
2022-06-02 05:13:54 +0000

    add homeroom options

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const viwe = document.getElementsByClassName("view")
+function init() {
+    if (!localStorage.getItem("formData")) {
+        changeView("signUp");
+    } else {
+        changeView("main");
+    }
+}
+
+function changeView(v) {
+    let views = document.getElementsByClassName("view");
+    for (let view of views) {
+        view.style.display = "none";
+    }
+    document.getElementById(v).style.display = "block";
+}
+
+// process when window is loaded
+
+window.onload = init();
